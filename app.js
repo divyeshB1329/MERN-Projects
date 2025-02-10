8e4bd05e902bcf64cb269bdeb8571bab2c53c35a
divyeshB1329 <dj.divyesh1304@gmail.com>
2025-02-11 00:11:04 +0530

    restructure image functionalty with cloudniary storage

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config()
+}
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -13,6 +17,7 @@ const passport = require("passport")
 const LocalStrategy = require("passport-local")
 const User =require("./models/user.js")
 const userRouter = require("./routes/user.js")
+
 
 const sessionOptions = {
     secret: "mysupersecretstring",
