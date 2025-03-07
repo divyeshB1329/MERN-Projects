c14a56c860d6c91d6948497f20385b9c56a6506b
divyeshB1329 <dj.divyesh1304@gmail.com>
2025-03-08 01:16:28 +0530

    Add Initial Files

--- app.js
+++ app.js
@@ -12,23 +12,17 @@ const ExpressError = require("./utils/ExpressError.js");
 const listingRouter = require("./routes/listing.js")
 const reviewRoter = require("./routes/review.js")
 const session = require("express-session");
+const MongoStore = require('connect-mongo')
 const flash = require("connect-flash")
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
 const User =require("./models/user.js")
-const userRouter = require("./routes/user.js")
+const userRouter = require("./routes/user.js");
+const { connect } = require('http2');
 
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+const dbUrl = process.env.ATLASDB_URL;
 
-const sessionOptions = {
-    secret: "mysupersecretstring",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    },
-}
 
 // ----------------------------- Configuration ---------------------------------- //
 
@@ -54,10 +48,35 @@ app.engine("ejs", ejsMate);
 //     res.locals.message = req.flash()
 // })
 
+
+const store = MongoStore.create({
+    mongoUrl:dbUrl,
+    crypto:{
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+})
+
+store.on("error", ()=>{
+    console.log("ERROR in MONGO SESSION STORE", err)
+})
+
+const sessionOptions = {
+    store,
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+}
+
 // ----------------------------- Database Connection ---------------------------- //
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect(dbUrl);
     console.log("Connected to DB");
 }
 
